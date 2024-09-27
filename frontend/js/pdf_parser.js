4f25e776c2a2e408b21560ed14ccc889112c2d8d
Anshuman Aryan <anshuman.aryan24@gmail.com>
2024-09-27 22:25:32 +0530

    Added flask app and JS logic to call PY code from JS

---
+++ frontend/js/pdf_parser.js
@@ -0,0 +1,10 @@
+function getFactorial() {
+    var number = "BCSE302P_DATABASE-SYSTEMS-LAB_LO_1.0_70_BCSE302P.pdf";
+    fetch(`http://127.0.0.1:5000/pdfparsingtool?number=${number}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log("object\n");
+            console.log(data.result);
+        })
+        .catch(error => console.error('Error:', error));
+}
