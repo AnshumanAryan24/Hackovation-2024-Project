f62d4bb4a746ad20832c72c8d1f2f05ad9227035
Anshuman Aryan <anshuman.aryan24@gmail.com>
2024-09-27 22:46:12 +0530

    Changing PDFParsing file path

--- frontend/js/pdf_parser.js
+++ frontend/js/pdf_parser.js
@@ -1,4 +1,4 @@
-function getFactorial() {
+function printPDF() {
     var number = "BCSE302P_DATABASE-SYSTEMS-LAB_LO_1.0_70_BCSE302P.pdf";
     fetch(`http://127.0.0.1:5000/pdfparsingtool?number=${number}`)
         .then(response => response.json())
@@ -8,3 +8,5 @@ function getFactorial() {
         })
         .catch(error => console.error('Error:', error));
 }
+
+printPDF()
