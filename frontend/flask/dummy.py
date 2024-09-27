6fd31c725b0e234a1a28d6c32cd86f1771e4019e
Sayar1234 <sayarch2003@gmail.com>
2024-09-28 03:16:04 +0530

    sample

--- frontend/flask/dummy.py
+++ frontend/flask/dummy.py
@@ -16,4 +16,4 @@ def parse():
 # print("Title: ", meta.title)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
--- frontend/js/pdf_parser.js
+++ frontend/js/pdf_parser.js
@@ -1,5 +1,5 @@
 function printPDF() {
-  var number = "BCSE302P_DATABASE-SYSTEMS-LAB_LO_1.0_70_BCSE302P.pdf";
+  var number = "BCSE209L_MACHINE-LEARNING_TH_1.0_71_BCSE209L_66 ACP.pdf";
   fetch(` http://0.0.0.0:5000/pdfparsingtool?number=${number}`)
     .then((response) => {
       console.log(response);
